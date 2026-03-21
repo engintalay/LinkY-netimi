38df0ab76f5f76cb842eba37d06167e2f1b90bb2
Engin Talay <engintalay@gmail.com>
2026-03-22 00:28:49 +0300

    Orijinal basit koda geri döndü - Instagram özel işlemleri kaldırıldı

--- includes/functions.php
+++ includes/functions.php
@@ -75,56 +75,34 @@ function fetchUrlDetails($url)
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    
-    // Instagram için özel ayarlar
-    if (strpos($url, 'instagram.com') !== false) {
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Language: en-US,en;q=0.5',
-            'Accept-Encoding: gzip, deflate',
-            'Connection: keep-alive',
-            'Cache-Control: no-cache',
-        ]);
-        curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
-    } else {
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; LinkManager/1.0)');
-    }
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; LinkManager/1.0)');
 
     $html = curl_exec($ch);
     
     $data = [
         'title' => '',
         'description' => '',
-        'images' => [],
-        'debug' => []
+        'images' => []
     ];
 
     if (curl_errno($ch)) {
         curl_close($ch);
         $data['title'] = parse_url($url, PHP_URL_HOST);
-        $data['error'] = 'CURL Error: ' . curl_error($ch);
         return $data;
     }
 
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
 
-    if ($httpCode !== 200) {
+    // Title
+    if (preg_match('/<title>(.*?)<\/title>/is', $html, $matches)) {
+        $data['title'] = trim($matches[1]);
+    } else {
         $data['title'] = parse_url($url, PHP_URL_HOST);
-        $data['error'] = "HTTP $httpCode";
-        $data['debug'][] = "HTTP Status: $httpCode";
-        return $data;
     }
 
-    $data['debug'][] = "HTML Length: " . strlen($html);
-    $data['debug'][] = "Contains og:image: " . (strpos($html, 'og:image') !== false ? 'Yes' : 'No');
-    $data['debug'][] = "Contains profile_pic_url: " . (strpos($html, 'profile_pic_url') !== false ? 'Yes' : 'No');
-    $data['debug'][] = "Contains fbcdn.net: " . (strpos($html, 'fbcdn.net') !== false ? 'Yes' : 'No');
-    $data['debug'][] = "Contains instagram.com/static: " . (strpos($html, 'instagram.com/static') !== false ? 'Yes' : 'No');
-    $data['debug'][] = "HTML Sample: " . substr($html, 0, 500) . "...";
+    // Get base URL for resolving relative URLs
+    $baseUrl = $url;
 
     // Description
     if (preg_match('/<meta name="description" content="(.*?)"/i', $html, $matches)) {
@@ -135,92 +113,16 @@ function fetchUrlDetails($url)
         $data['description'] = trim($matches[1]);
     }
 
-    // Title
-    if (strpos($url, 'instagram.com') !== false) {
-        // Instagram için özel parsing
-        if (preg_match('/instagram\.com\/([^\/\?]+)/', $url, $matches)) {
-            $username = $matches[1];
-            $data['title'] = "@$username - Instagram";
-        }
-        
-        // Instagram JSON data'sını bul
-        if (preg_match('/window\._sharedData = ({.*?});/', $html, $jsonMatches)) {
-            $jsonData = json_decode($jsonMatches[1], true);
-            if (isset($jsonData['entry_data']['ProfilePage'][0]['graphql']['user'])) {
-                $user = $jsonData['entry_data']['ProfilePage'][0]['graphql']['user'];
-                if (!empty($user['full_name'])) {
-                    $data['title'] = $user['full_name'] . " (@" . $user['username'] . ") - Instagram";
-                }
-            }
-        }
-    } else {
-        // Normal title parsing
-        if (preg_match('/<title>(.*?)<\/title>/is', $html, $matches)) {
-            $data['title'] = trim($matches[1]);
-        } else {
-            $data['title'] = parse_url($url, PHP_URL_HOST);
-        }
-    }
-
-    // Get base URL for resolving relative URLs
-    $baseUrl = $url;
-
     // Images (og:image)
     if (preg_match_all('/<meta property="og:image" content="(.*?)"/i', $html, $matches)) {
         foreach($matches[1] as $img) {
-            $imgUrl = makeAbsoluteUrl($img, $baseUrl);
-            
-            // Instagram: Try to get higher resolution version
-            if (strpos($url, 'instagram.com') !== false && strpos($imgUrl, 'scontent') !== false) {
-                // Keep original small image and also add larger version
-                $data['images'][] = $imgUrl; // Original
-                
-                // Try larger version
-                $largeUrl = preg_replace('/\/s\d+x\d+\//', '/s1080x1080/', $imgUrl);
-                if ($largeUrl !== $imgUrl) {
-                    $data['images'][] = $largeUrl;
-                }
-            } else {
-                $data['images'][] = $imgUrl;
-            }
+            $data['images'][] = makeAbsoluteUrl($img, $baseUrl);
         }
     }
     // twitter:image
     if (preg_match_all('/<meta name="twitter:image" content="(.*?)"/i', $html, $matches)) {
         foreach($matches[1] as $img) {
-            $imgUrl = makeAbsoluteUrl($img, $baseUrl);
-            $data['images'][] = $imgUrl;
-        }
-    }
-    // Instagram: Also try property="twitter:image"
-    if (strpos($url, 'instagram.com') !== false) {
-        if (preg_match_all('/<meta property="twitter:image" content="(.*?)"/i', $html, $matches)) {
-            foreach($matches[1] as $img) {
-                $imgUrl = makeAbsoluteUrl($img, $baseUrl);
-                $data['images'][] = $imgUrl;
-            }
-        }
-        
-        // Try to find profile_pic_url in JSON data
-        if (preg_match('/"profile_pic_url":"([^"]+)"/', $html, $matches)) {
-            $profilePic = str_replace('\/', '/', $matches[1]);
-            $data['images'][] = $profilePic;
-        }
-        
-        // Try to find any instagram CDN images
-        if (preg_match_all('/https:\/\/[^"]*\.cdninstagram\.com[^"]*\.jpg/', $html, $matches)) {
-            foreach($matches[0] as $img) {
-                $data['images'][] = $img;
-            }
-        }
-        
-        // Try to find fbcdn.net images (new Instagram CDN)
-        if (preg_match_all('/https:\/\/[^"]*\.fbcdn\.net[^"]*\.jpg[^"]*/', $html, $matches)) {
-            foreach($matches[0] as $img) {
-                // Clean up HTML entities
-                $img = html_entity_decode($img);
-                $data['images'][] = $img;
-            }
+            $data['images'][] = makeAbsoluteUrl($img, $baseUrl);
         }
     }
     // link rel="image_src"
